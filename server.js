738d7b4e1f4b8c7e4a5bbbb6d06d40d19848337b
Parv Patel <101592578+parvpatel2609@users.noreply.github.com>
2023-08-05 10:41:28 +0530

    Updated server.js

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-import express  from "express";
+import express from "express";
 import dotenv from "dotenv";
 import morgan from "morgan";
 import connectDB from "./config/db.js";
@@ -7,6 +7,8 @@ import courseRoutes from "./routes/courseRoutes.js";
 import gradeRoutes from './routes/gradeRoutes.js';
 import eventRoutes from './routes/eventRoutes.js';
 import cors from "cors";
+import path from "path";
+import { fileURLToPath } from "url";
 
 //configure env 
 dotenv.config();
@@ -15,15 +17,18 @@ dotenv.config();
 //database config
 connectDB();
 
-
+//esmodule fix
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 //rest object
-const app = express(); 
+const app = express();
 
 
 //middlewares
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
+app.use(express.static(path.join(__dirname, "./client/build")));
 
 
 //routes
@@ -41,7 +46,7 @@ app.get("/", (req, res) => {
 
 
 //port
-const PORT = process.env.PORT || 8080; 
+const PORT = process.env.PORT || 8080;
 
 
 //run listen 
